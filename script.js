dcee60b7f58a85009f1c780e7a53a113e646fbe3
samuel tedeschi <samueltedeschi100@yahoo.com>
2022-05-13 15:39:41 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const contener = document.querySelector('.contener')
-const teclado = document.querySelector('.teclado')
+const calculator = document.querySelector('.calculator')
+const keys = document.querySelector('.calculator__keys')
+const display = document.querySelector('.calculator__display')
 
-teclado.addEventListener('click', e => {
+keys.addEventListener('click', e => {
     if(e.target.matches('button')){
         const key = e.target
-        const action = key.dataset.action 
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent 
+        const previousKeyType = calculator.dataset.previousKeyType
         
         if(!action){
             console.log('numero')
@@ -16,10 +20,16 @@ teclado.addEventListener('click', e => {
             action === 'subtract' ||
             action === 'divide' 
         ){
+            key.classList.add('is-depressed')
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+
             console.log('operadores')
         }
-
+        
         if(action === 'decimal'){
+            display.textContent = displayedNum + '.'
             console.log('decimal')
         }
 
@@ -29,9 +39,47 @@ teclado.addEventListener('click', e => {
 
         if(action === 'calculate'){
             console.log('igual')
-        }
-    }
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
 
+            display.textContent = calculate(firstValue, operator, secondValue)
+            const calculate = (n1, operator, n2) =>{
+                let result =''
+
+                if (operator === 'add') {
+                    result = parseFloat(n1) + parseFloat(n2)
+                } else if (operator === 'multiply'){
+                    result = parseFloat(n1) * parseFloat(n2)
+                } else if (operator === 'subtract'){
+                    result = parseFloat(n1) - parseFloat(n2) 
+                } else if (operator === 'divide'){
+                    result = parseFloat(n1) / parseFloat(n2) 
+                }
+
+                return result
+            }
+        }
+
+        if(!action){
+            if(displayedNum === '0'){
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
+        
+        if(!action){
+            if(displayedNum === '0' || previousKeyType === 'operator'){
+                display.textContent = keyContent
+            }else{
+                display.textContent = displayedNum + keyContent
+            }
+        }
+
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depresset'))
+    }
+    
 })
 
 
